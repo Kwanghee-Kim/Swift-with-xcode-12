d223e5f225f0aa8af270ad623ed609a628b4e97c
Kwanghee-Kim <61907171+Kwanghee-Kim@users.noreply.github.com>
2021-05-12 00:39:56 +0900

    Optional Binding (if 를 이용한 optional unwrapping)

--- HelloSwiftProject/ViewController.swift
+++ HelloSwiftProject/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBAction func Click_MoveBtn(_ sender: Any) {
+      
+        // if 를 통해 unwrap (optional binding)
+        // 문법_1 : if let constantname = someOptional { }  <- someOptional 에 값이 있을 경우 중괄호 안의 task 를 실행
+        // 문법_2 : if let constantname = someOptional { } else {}  <- else 사용 가능
+        // 문법_3 : if A != nil { let constantname = A! }
+        
         if let controller = self.storyboard?.instantiateViewController(withIdentifier: "DetailController"){
             self.navigationController?.pushViewController(controller, animated: true)
+            
+        // self : 나 자신을 참조
         }
    
     }
